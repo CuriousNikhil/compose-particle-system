f002aa168d4a1c876072fc9e7666f6ddfde208a0
Matt Smith <smithyis1337@hotmail.com>
2022-07-11 02:42:32 +0100

    Annotation fix

--- app/src/main/java/me/nikhilchaudhari/composeparticlesystem/ui/theme/ComposeparticlesystemTheme.kt
+++ app/src/main/java/me/nikhilchaudhari/composeparticlesystem/ui/theme/ComposeparticlesystemTheme.kt
@@ -30,9 +30,9 @@ private val LightColorPalette = lightColors(
 @Composable
 fun ComposeparticlesystemTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-)
-{
+    content:
+    @Composable() () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
